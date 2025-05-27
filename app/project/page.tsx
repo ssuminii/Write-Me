d02d301152ce4c0f04fa5ad0f12d72088978cee0
Sumin <tnalsdldkr2@naver.com>
2025-05-28 03:38:36 +0900

    feat: drag and drop으로 markdown 정렬 순서 교체 (#15)

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -5,15 +5,29 @@ import Markdown from '@/components/markdown'
 import { projectCards } from './data/project-data'
 import { useProjectForm } from './_hooks/useProjectForm'
 import { getMarkdownFromCards } from '@/utils/markdown'
+import { useState, useEffect } from 'react'
+import type { UniqueIdentifier } from '@dnd-kit/core'
 
 export default function Project() {
+  const [order, setOrder] = useState<UniqueIdentifier[]>([])
   const { state, handlers, collapsedMap } = useProjectForm()
   const cards = projectCards(state, handlers, collapsedMap)
-  const markdown = getMarkdownFromCards(cards)
+  const orderedCards = order.map((id) => cards.find((card) => card.id === id)!).filter(Boolean)
+
+  const markdown = getMarkdownFromCards(orderedCards)
+
+  useEffect(() => {
+    setOrder(cards.map((card) => card.id))
+  }, [])
 
   return (
     <div className='flex w-full min-h-screen py-4'>
-      <DndCardList className='flex-1' items={cards} onToggleCollapse={handlers.onToggleCollapse} />
+      <DndCardList
+        className='flex-1'
+        items={cards}
+        onToggleCollapse={handlers.onToggleCollapse}
+        onReorder={setOrder}
+      />
       <Markdown className='flex-2' value={markdown} onChange={() => {}} />
     </div>
   )
