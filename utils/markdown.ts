4715258c200661a6106f9dff30a7e3964bb698d7
Sumin <tnalsdldkr2@naver.com>
2025-05-27 17:20:15 +0900

    refactor: getMarkdownFromCards 함수에서 카드 간 시각적 간격을 위해 <br> 태그 추가 (#15)

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,4 +1,4 @@
 import type { CardItem } from '@/components/dnd/card-list'
 
 export const getMarkdownFromCards = (cards: CardItem[]) =>
-  cards.map((c) => c.markdown?.trim()).filter(Boolean).join('\n\n')
+  cards.map((c) => c.markdown?.trim()).filter(Boolean).join('\n\n<br>\n\n')
