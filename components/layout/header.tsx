cc812b2ccadb46e31bc0a0a282cafefea1a41168
Sumin <tnalsdldkr2@naver.com>
2025-06-01 17:29:11 +0900

    feat: 로그아웃 기능 구현 (#17)

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,48 @@
+'use client'
+
 import MainNav from '@/components/layout/main-nav'
 import ModeToggle from '@/components/mode-toggle'
+import { supabase } from '@/lib/supabaseClient'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+  const [isLogin, setIsLogin] = useState(false)
+
+  useEffect(() => {
+    const getSession = async () => {
+      const { data } = await supabase.auth.getSession()
+      setIsLogin(!!data.session)
+    }
+    getSession()
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setIsLogin(!!session)
+    })
+
+    return () => {
+      listener.subscription.unsubscribe()
+    }
+  }, [])
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut()
+  }
+
   return (
     <header className='flex justify-between items-center h-22 px-12 font-semibold border-b border-gray-300'>
       <MainNav />
       <nav className='flex items-center gap-4'>
-        <Link href='/login' className=' hover:text-primary-hover'>
-          Login
-        </Link>
+        {isLogin ? (
+          <div onClick={handleLogout} className=' hover:text-primary-hover'>
+            Logout
+          </div>
+        ) : (
+          <Link href='/login' className=' hover:text-primary-hover'>
+            Login
+          </Link>
+        )}
+
         <ModeToggle />
       </nav>
     </header>
