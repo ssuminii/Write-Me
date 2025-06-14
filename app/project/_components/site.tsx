d94cb832699410b7dde52e5acf67830d284e0141
Sumin <tnalsdldkr2@naver.com>
2025-06-14 22:23:49 +0900

    refactor: 프로젝트 링크 Input을 LabelInput 컴포넌트로 교체 (#26)

--- app/project/_components/site.tsx
+++ app/project/_components/site.tsx
@@ -1,34 +1,23 @@
-import { Input } from '@/components/ui'
+import { LabelInput } from '@/components/ui'
 import type { SiteProps } from '@/app/project/_models/site'
 
 const Site = ({ name, link, onChange }: SiteProps) => {
   return (
     <div className='flex flex-col gap-6'>
-      <div className='flex items-center gap-4'>
-        <label htmlFor='project-name' className='w-24'>
-          프로젝트 이름
-        </label>
-        <Input
-          id='project-name'
-          value={name}
-          onChange={(e) => onChange({ name: e.target.value, link })}
-          placeholder='Write Me'
-          className='flex-1'
-        />
-      </div>
-
-      <div className='flex items-center gap-4'>
-        <label htmlFor='project-link' className='w-24'>
-          프로젝트 링크
-        </label>
-        <Input
-          id='project-link'
-          value={link}
-          onChange={(e) => onChange({ name, link: e.target.value })}
-          placeholder='wwww.writeme.com'
-          className='flex-1'
-        />
-      </div>
+      <LabelInput
+        label='프로젝트 이름'
+        pos='row'
+        placeholder='Write Me'
+        value={name}
+        onChange={(value) => onChange({ name: value, link })}
+      />
+      <LabelInput
+        label='프로젝트 링크'
+        pos='row'
+        placeholder='www.writeme.com'
+        value={link}
+        onChange={(value) => onChange({ name, link: value })}
+      />
     </div>
   )
 }
