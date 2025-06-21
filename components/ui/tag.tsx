317e67a67c0789d585250ce41e1953ceb28bdae1
Sumin <tnalsdldkr2@naver.com>
2025-06-21 19:36:59 +0900

    feat: 해시태그 컴포넌트 구현 (#23)

---
+++ components/ui/tag.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx'
+
+interface TagProps {
+  label: string
+  selected?: boolean
+  onClick?: () => void
+}
+
+export const Tag = ({ label, selected = false, onClick }: TagProps) => {
+  return (
+    <button
+      type='button'
+      onClick={onClick}
+      className={clsx(
+        'w-fit text-sm px-4 py-1 rounded-full border transition-colors',
+        selected
+          ? 'bg-primary text-white border-primary'
+          : 'hover:bg-primary hover:text-white hover:cursor-pointer'
+      )}
+    >
+      # {label}
+    </button>
+  )
+}
