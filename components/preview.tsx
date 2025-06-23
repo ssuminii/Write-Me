6d627b8d41eb5a7cf5df78286763757a3891d382
Sumin <tnalsdldkr2@naver.com>
2025-06-23 21:28:37 +0900

    refactor: Markdown preview를 클라이언트 컴포넌트로 분리하여 SSR 최적화 (#38)

---
+++ components/preview.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import MDEditor from '@uiw/react-md-editor'
+
+interface PreviewProps {
+  source: string
+}
+
+const Preview = ({ source }: PreviewProps) => {
+  const { theme, systemTheme } = useTheme()
+
+  const currentTheme = theme === 'system' ? systemTheme : theme
+  const mode = currentTheme === 'dark' ? 'dark' : 'light'
+
+  return (
+    <section data-color-mode={mode} className='border p-8'>
+      <MDEditor.Markdown source={source} />
+    </section>
+  )
+}
+
+export default Preview
