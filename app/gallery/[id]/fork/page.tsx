b9124612f68d386c19e68543a2897ac96e699224
Sumin <tnalsdldkr2@naver.com>
2025-06-24 20:24:41 +0900

    feat: README 마크다운 편집 기능 추가 (#44)

--- app/gallery/[id]/fork/page.tsx
+++ app/gallery/[id]/fork/page.tsx
@@ -1,20 +1,21 @@
+'use client'
+
 import Markdown from '@/components/markdown'
 import { Info } from './_components'
 import FileActions from '@/components/file-actions'
+import { useState } from 'react'
+import { mockReadme } from '@/mocks/gallery'
 
 const Page = () => {
-  const data = {
-    user: 'ssuminii',
-    markdown: '# title',
-  }
+  const [markdown, setMarkdown] = useState<string>(mockReadme.source ?? '')
 
   return (
     <div className='flex flex-col gap-6 py-6 h-full'>
       <div className='flex items-center justify-between mx-10'>
-        <Info user={data.user} />
-        <FileActions markdown={''} />
+        <Info user={mockReadme.author} />
+        <FileActions markdown={markdown} />
       </div>
-      <Markdown value={data.markdown} className='flex-1' />
+      <Markdown value={markdown} onChange={(value) => setMarkdown(value)} className='flex-1' />
     </div>
   )
 }
