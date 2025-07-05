30a1826faaceae576ef410cd8e33bcd7f6d02d58
Sumin <tnalsdldkr2@naver.com>
2025-07-05 13:58:15 +0900

    feat: Gallery 페이지 검색 기능 구현 및 검색어 필터링 로직 추가 (#42)

--- app/gallery/_components/search-bar.tsx
+++ app/gallery/_components/search-bar.tsx
@@ -1,15 +1,41 @@
+'use client'
+
 import { Input } from '@/components/ui'
 import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
 
 const SearchBar = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const [keyword, setKeyword] = useState(searchParams.get('q') || '')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const params = new URLSearchParams(searchParams)
+    if (keyword) {
+      params.set('q', keyword)
+    } else {
+      params.delete('q')
+    }
+
+    router.push(`?${params.toString()}`)
+  }
+
   return (
-    <div className='relative'>
+    <form onSubmit={handleSubmit} className='relative'>
       <Search
         size={16}
         className='absolute left-6 top-1/2 -translate-y-1/2 text-muted-foreground'
       />
-      <Input className='w-xl rounded-full pl-12' placeholder='찾으시는 README를 입력해주세요.' />
-    </div>
+      <Input
+        className='w-xl rounded-full pl-12'
+        placeholder='찾으시는 README를 입력해주세요.'
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+    </form>
   )
 }
 
