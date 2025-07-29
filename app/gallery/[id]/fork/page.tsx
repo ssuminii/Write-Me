0108ac8645e356ea924f28ff1fd754b2731d3197
Sumin <tnalsdldkr2@naver.com>
2025-07-29 10:29:54 +0900

    fix: fork 페이지 데이터 연동 수정

--- app/gallery/[id]/fork/page.tsx
+++ app/gallery/[id]/fork/page.tsx
@@ -4,18 +4,24 @@ import Markdown from '@/components/markdown'
 import { Info } from './_components'
 import FileActions from '@/components/file-actions'
 import { useState } from 'react'
-import { mockReadme } from '@/mocks/gallery'
+import { useReadmeByIdQuery } from '@/hooks/queries'
+import { useParams } from 'next/navigation'
 
 const Page = () => {
-  const [markdown, setMarkdown] = useState<string>(mockReadme.source ?? '')
+  const params = useParams()
+  const id = params?.id as string
+
+  const { data: readme } = useReadmeByIdQuery(id)
+
+  const [markdown, setMarkdown] = useState<string>(readme?.source ?? '')
 
   return (
     <div className='flex flex-col gap-6 py-6 h-full'>
-      <div className='flex items-center justify-between mx-10'>
-        <Info user={mockReadme.author} />
+      <div className='flex items-center justify-between mx-10 '>
+        <Info user={readme?.author ?? ''} />
         <FileActions markdown={markdown} />
       </div>
-      <Markdown value={markdown} onChange={(value) => setMarkdown(value)} className='flex-1' />
+      <Markdown value={markdown} onChange={(value) => setMarkdown(value)} className='flex-1 px-2' />
     </div>
   )
 }
